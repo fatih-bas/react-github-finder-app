51d51365f0ee717bb22ddb3892236fced71fd2d0
fatih-bas <fatihbas1@gmail.com>
2022-07-05 22:50:45 +0300

    added modified .env file

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -22,7 +22,6 @@ const UserSearch = () => {
       setText("")
     }
   }
- 
   return (
     <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
       <div>
