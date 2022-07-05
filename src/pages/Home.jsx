572cb724e42effdebefb032c15fb99ab2e000019
fatih-bas <fatihbas1@gmail.com>
2022-07-05 21:32:18 +0300

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import UserResults from "../components/users/UserResults"
+import UserSearch from "../components/users/UserSearch"
+
+const Home = () => {
+  return (
+    <>
+      <UserSearch />
+      <UserResults />
+      
+    </>
+  )
+}
+
+export default Home
